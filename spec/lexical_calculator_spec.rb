8bd15a3b3cb835ea6ecc743d52951255e758d859
Uri Mikhli <urimikhli@gmail.com>
2015-03-03 20:54:46 -0500

    test for basic behavior of LexicalCalculator

---
+++ spec/lexical_calculator_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../lexical_calculator.rb'
+
+describe LexicalCalculator do
+  it "should have a calculate method" do
+    expect(LexicalCalculator).to respond_to(:calculate)
+  end
+  it "should do math correctly" do
+    expect(LexicalCalculator.calculate("12 + 33")).to be(45.to_f)
+    expect(LexicalCalculator.calculate("12 - 33")).to be(-21.to_f)
+    expect(LexicalCalculator.calculate("12 * 33")).to be(396.to_f)
+    expect(LexicalCalculator.calculate("12 / 33")).to be(0.36363636363636365.to_f)
+    
+  end
+end
